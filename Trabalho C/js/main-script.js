b444b76ccdc70843cb089395677d7334202479ee
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-05-14 18:03:26 +0100

    added central cylinder

--- Trabalho C/js/main-script.js
+++ Trabalho C/js/main-script.js
@@ -4,23 +4,58 @@ import { VRButton } from 'three/addons/webxr/VRButton.js';
 import * as Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
+const radialSegments = 16;
+
+const cylinderHeight = 50;
+const cylinderRadius = 10;
+const ringRadius = 20;
+const innerRingHeight = cylinderHeight * 3 / 4;
+const innerRingInnerRadius = cylinderRadius;
+const innerRingOuterRadius = innerRingInnerRadius + ringRadius;
+const middleRingHeight = cylinderHeight * 2 / 4;
+const middleRingInnerRadius = innerRingOuterRadius;
+const middleRingOuterRadius = middleRingInnerRadius + ringRadius;
+const outerRingHeight = cylinderHeight * 1 / 4;
+const outerRingInnerRadius = middleRingOuterRadius;
+const outerRingOuterRadius = outerRingInnerRadius + ringRadius;
+
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-
+var camera, scene, renderer;
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
 function createScene(){
     'use strict';
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xe6ffe6);
 
+    scene.add(new THREE.AxesHelper(10));
+
+    createCarousel();
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function createCamera() {
+    'use strict';
 
+    // FIZ ESTA BOMBA SÒ PARA TESTAR, MUDEM DEPOIS
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var near = 1;
+    var far = 1000;
+    var fov = 70;
+    camera = new THREE.PerspectiveCamera(fov, width / height, near, far);
+    camera.position.x = 100;
+    camera.position.y = 100;
+    camera.position.z = 100;
+    camera.lookAt(scene.position);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -29,6 +64,47 @@ function createScene(){
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+function createCarousel() {
+    var carousel = new THREE.Object3D();
+
+    //createCentralCylinder(carousel, 0, 0, 0);
+    createInnerRing(carousel, 0, 0, 0);
+
+    scene.add(carousel);
+}
+
+function createCentralCylinder(parent, x, y, z) {
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+
+    var path = new VerticalLine();
+    var tubeGeometry = new THREE.TubeGeometry(path, 1, cylinderRadius, radialSegments, false);
+    var mesh = new THREE.Mesh(tubeGeometry, material);
+    mesh.position.set(x, y, z);
+
+    parent.add(mesh);
+}
+
+class VerticalLine extends THREE.Curve {
+
+    getPoint(t, optionalTarget = new THREE.Vector3()) {
+        return optionalTarget.set(0, t, 0).multiplyScalar(cylinderHeight);
+    }
+
+}
+
+function createInnerRing(parent, x, y, z) {
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+
+    var ringGeometry = new THREE.RingGeometry(innerRingInnerRadius, innerRingOuterRadius, radialSegments);
+    //var ringWithHeightGeometry = new THREE.ExtrudeGeometry(ringGeometry, {depth: innerRingHeight, bevelEnabled: false});
+
+    var mesh = new THREE.Mesh(ringGeometry, material);
+    mesh.rotation.set(Math.PI / 2, 0, 0);
+    mesh.position.set(x, y, z);
+
+    parent.add(mesh);
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -59,7 +135,7 @@ function update(){
 /////////////
 function render() {
     'use strict';
-
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -67,7 +143,19 @@ function render() {
 ////////////////////////////////
 function init() {
     'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
+    createScene();
+    createCamera();
+
+    render();
+
+    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener('keyup', onKeyUp);
 }
 
 /////////////////////
@@ -76,6 +164,9 @@ function init() {
 function animate() {
     'use strict';
 
+    render();
+
+    requestAnimationFrame(animate);
 }
 
 ////////////////////////////
@@ -89,8 +180,21 @@ function onResize() {
 ///////////////////////
 /* KEY DOWN CALLBACK */
 ///////////////////////
+const angleStep = Math.PI / 180; // Adjust as needed
+const angle1Range = { min: -Math.PI / 4, max: Math.PI / 4 }; // Example range
 function onKeyDown(e) {
     'use strict';
+
+    switch (e.keyCode) {
+    case 81: //Q
+    case 113: //q
+        if ((scene.rotation.y + angleStep) != angle1Range.max) {
+            const rotationMatrix = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), angleStep);
+            scene.applyMatrix4(rotationMatrix);
+        }
+        break;
+    default:
+  }
 
 }
 
